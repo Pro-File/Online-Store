b5a661c6f68307ef062bec5b0ab0e539a0f28013
Pro-File <team.logicart@gmail.com>
2020-12-11 23:05:50 +0500

    done with initial web skeleton

--- src/App.js
+++ src/App.js
@@ -1,9 +1,25 @@
 import './App.css';
+import {Route, Switch} from 'react-router-dom';
+import Home from './Pages/home/Home';
+import Category from './Pages/category/Category';
+import Authentication from './Pages/authentication/Authentication';
+import CategoryProducts from './Pages/category-products/CategoryProducts';
+import Checkout from './Pages/checkout/Checkout';
+
+
 
 function App() {
   return (
     <div className="App">
      <h1>Ecommerce Website</h1>
+     <Switch>
+       <Route path="/" component = {Home} exact/>
+       <Route path="/auth" component = {Authentication}/>
+       <Route path="/categories" component = {Category}/>
+       <Route path="/category-products" component = {CategoryProducts}/>
+       <Route path="/checkout" component = {Checkout}/>
+
+     </Switch>
     </div>
   );
 }
